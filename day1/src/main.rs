4f7fa51788b4af4965ac7029370ef3e89b6a5a01
Harry van Wier <harryvanwier@gmail.com>
2021-12-01 18:32:42 +0100

    Add day 1 solutions

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("reading input file failed");
+    let input = input.lines();
+    let input : Vec<u32> = input.map(|n| n.parse::<u32>().unwrap()).collect();
+
+    let ans1 = input.windows(2).filter(|w| w[1] > w[0]).count();
+    println!("Numbet of increasing numbers is {}!", ans1);
+
+    let sum_of_windows : Vec<u32> = input.windows(3).map(|w| w.iter().sum()).collect();
+    let ans2 = sum_of_windows.windows(2).filter(|w| w[1] > w[0]).count();
+    println!("Numbet of increasing windows is {}!", ans2);
 }
