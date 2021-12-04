c66baeb795af959dba84b66041b32dfbd10ab01b
Harry van Wier <harryvanwier@gmail.com>
2021-12-04 01:45:21 +0100

    Add day2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,11 +17,34 @@ fn main() {
         }).unwrap()
     }).collect();
 
+    // Part 1
     let mut command_sum : HashMap<String, u32> = HashMap::new();
-    for (command, count) in input {
-        *command_sum.entry(command).or_insert(0) += count;
+    for (command, count) in &input {
+        *command_sum.entry(command.to_string()).or_insert(0) += count;
     }
 
     let depth = command_sum.get("down").unwrap() - command_sum.get("up").unwrap();
     println!("Part 1: depth * horizontal position = {}", depth * command_sum.get("forward").unwrap());
+
+    // Part 2
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut horizontal_pos = 0;
+    for (command, count) in input {
+        match command.as_str() {
+            "down" => {
+                aim += count;
+            },
+            "up" => {
+                aim -= count;
+            },
+            "forward" => {
+                horizontal_pos += count;
+                depth += aim * count;
+            },
+            _ => { panic!(); }
+        }
+    }
+
+    println!("Part 2: depth * horizontal position = {}", depth * horizontal_pos);
 }
