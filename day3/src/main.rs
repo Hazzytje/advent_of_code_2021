204032456e4e512a7898539d1d381ba8c175cd72
Harry van Wier <harryvanwier@gmail.com>
2021-12-04 21:57:49 +0100

    Add day 3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("reading input file failed");
+    let input = input.lines();
+
+    let number_of_bits = input.clone().next().unwrap().chars().count();
+
+    let mut bit_counts : Vec<HashMap<char, u32>> = Vec::new();
+
+    for _ in 0..number_of_bits {
+        bit_counts.push(HashMap::new());
+    }
+
+    for line in input {
+        for (i, c) in line.chars().enumerate() {
+            *bit_counts[i].entry(c).or_default() += 1;
+        }
+    }
+
+    let mut most_common_bits = 0;
+    let mut least_common_bits = 0;
+
+    for bit_count in bit_counts {
+        most_common_bits <<= 1;
+        least_common_bits <<= 1;
+
+        if bit_count.get(&'1').unwrap() > bit_count.get(&'0').unwrap() {
+            most_common_bits |= 1;
+        } else {
+            least_common_bits |= 1;
+        }
+    }
+
+    println!("least * most common bits = {}", most_common_bits * least_common_bits);
 }
